c086fd7c91bf4fadb7d931556a8d5c00fbef70b6
Petro Krevenets <peter.k.nets@gmail.com>
2026-08-18 13:10:02 +0300

    Improve tests

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -7,11 +7,29 @@ import { EpisodesService } from './episodes.service';
 describe('EpisodesController', () => {
   let controller: EpisodesController;
 
+  const mockFindAll = jest.fn();
+  const mockFindFeatured = jest.fn();
+  const mockFindOne = jest.fn();
+  const mockCreate = jest.fn();
+  const mockUpdate = jest.fn();
+  const mockDelete = jest.fn();
+
+  const mockEpisodesService = {
+    findAll: mockFindAll,
+    findFeatured: mockFindFeatured,
+    findOne: mockFindOne,
+    create: mockCreate,
+    update: mockUpdate,
+    delete: mockDelete,
+  };
+
   beforeEach(async () => {
+    jest.resetAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       imports: [ConfigModule],
       controllers: [EpisodesController],
-      providers: [EpisodesService],
+      providers: [{ provide: EpisodesService, useValue: mockEpisodesService }],
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
@@ -19,5 +37,114 @@ describe('EpisodesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    const mockResult = [
+      { id: 'id-1', name: 'Episode 1' },
+      { id: 'id-2', name: 'Episode 2' },
+    ];
+
+    beforeEach(() => {
+      mockFindAll.mockResolvedValue(mockResult);
+    });
+
+    it('should return the correct response', async () => {
+      const result = await controller.findAll();
+
+      expect(result).toEqual(mockResult);
+    });
+  });
+
+  describe('mockFindFeatured', () => {
+    const mockResult = { id: 'id-2', name: 'Episode 2', featured: true };
+
+    beforeEach(() => {
+      mockFindFeatured.mockResolvedValue(mockResult);
+    });
+
+    it('should return the correct response', async () => {
+      const result = await controller.findFeatured();
+
+      expect(result).toEqual(mockResult);
+    });
+  });
+
+  describe('findOne', () => {
+    const episodeId = 'id';
+    const mockResult = { id: episodeId, name: 'New episode' };
+
+    beforeEach(() => {
+      mockFindOne.mockResolvedValue(mockResult);
+    });
+
+    it('should call the findOne method with the correct parameters', async () => {
+      await controller.findOne(episodeId);
+      expect(mockFindOne).toHaveBeenCalledWith(episodeId);
+    });
+
+    it('should return the correct response', async () => {
+      const result = await controller.findOne(episodeId);
+
+      expect(result).toEqual(mockResult);
+    });
+  });
+
+  describe('create', () => {
+    const mockResult = { id: 'id', name: 'New episode' };
+    const mockBody = { name: 'New episode' };
+
+    beforeEach(() => {
+      mockCreate.mockResolvedValue(mockResult);
+    });
+
+    it('should call the create method with the correct parameters', async () => {
+      await controller.create(mockBody);
+      expect(mockCreate).toHaveBeenCalledWith(mockBody);
+    });
+
+    it('should return the correct response', async () => {
+      const result = await controller.create(mockBody);
+      expect(result).toEqual(mockResult);
+    });
+  });
+
+  describe('update', () => {
+    const episodeId = 'id';
+    const mockResult = { id: episodeId, name: 'Updated episode' };
+    const mockBody = { name: 'Updated episode' };
+
+    beforeEach(() => {
+      mockUpdate.mockResolvedValue(mockResult);
+    });
+
+    it('should call the update method with the correct parameters', async () => {
+      await controller.update(episodeId, mockBody);
+      expect(mockUpdate).toHaveBeenCalledWith(episodeId, mockBody);
+    });
+
+    it('should return the correct response', async () => {
+      const result = await controller.update(episodeId, mockBody);
+      expect(result).toEqual(mockResult);
+    });
+  });
+
+  describe('delete', () => {
+    const episodeId = 'id';
+    const mockResult = { id: episodeId, name: 'Deleted episode' };
+
+    beforeEach(() => {
+      mockDelete.mockResolvedValue(mockResult);
+    });
+
+    it('should call the delete method with the correct parameters', async () => {
+      await controller.delete(episodeId);
+      expect(mockDelete).toHaveBeenCalledWith(episodeId);
+    });
+
+    it('should return the correct response', async () => {
+      const result = await controller.delete(episodeId);
+      expect(result).toEqual(mockResult);
+    });
   });
 });
